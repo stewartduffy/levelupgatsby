e39fda24d502e1493bff39a7864b32e9d2a42c75
Stewart Duffy <stewart.duffy@paymark.co.nz>
2018-03-20 07:12:16 +1300

    Adds path prefix & gh-pages deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: 'Gatsby Default Starter',
     description: 'My first Gatsby site!',
   },
+  pathPrefix: '/gatsby',
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
